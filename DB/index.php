4c3bf095a4030f756dcb7054f51c82675c7c9870
Marco_Berti <ber.bertino@gmail.com>
2019-01-21 17:36:16 +0100

    insert into database php page works

--- DB/index.php
+++ DB/index.php
@@ -1,49 +1,182 @@
-<h1>Cacca</h1>
-<p>Ciao</p>
-<p style="color:red;">
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <style>
+        .error {color: #FF0000;}
+    </style>
+</head>
+
+<body>
 <?php
-$servername = "localhost";
-$username = "xsigomft_bestore";
-$password = "bertistore;2019";
-$dbname = "xsigomft_bestore";
-
-// function cripta_risultati($testo_in_chiaro){
-//     global $key_cookie_id;
-//     $ivlen = openssl_cipher_iv_length($cipher="AES-256-CBC");
-//     $iv = openssl_random_pseudo_bytes($ivlen);
-//     $ciphertext_raw = openssl_encrypt($testo_in_chiaro, $cipher, $key_cookie_id, $options=OPENSSL_RAW_DATA, $iv);
-//     $hmac = hash_hmac('sha256', $ciphertext_raw, $key_cookie_id, $as_binary=true);
-//     return base64_encode( $iv.$hmac.$ciphertext_raw );
-//   }
-
-// $stringa = $_POST["query"];
-// $stringa;
-
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connessione riuscita\n";
-
-$sql = "UPDATE oggetto SET nome = 'microfono' WHERE id = 'bestore_3'";
-echo "</br>";
-var_dump($sql);
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        foreach($row as $value)
-            echo $value . "</br>";
+    $servername = "localhost";
+    $username = "xsigomft_bestore";
+    $password = "bertistore;2019";
+    $dbname = "xsigomft_bestore";
+    
+    $nameErr = $idErr = $marcaErr = $annoErr = $lunghezzaErr = $posizioneErr = $pwdErr = "";
+    $pwd = $nome = $id = $marca = $anno = $lunghezza = $posizione = $descrizione = "";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    //echo "Connessione riuscita</br>";
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST['id'])) {
+            $idErr = "Richiesto";
+        }
+        else {
+            $id = test_input($_POST['id']);
+        }
     }
-} 
-else {
-    echo "0 results";
-}
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST['name'])) {
+            $nameErr = "Richiesto";
+        }
+        else {
+            $nome = test_input($_POST['name']);
+        }
+    }
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST['brand'])) {
+            $marca = "";
+        }
+        else {
+            $marca = test_input($_POST['brand']);
+        }
+    }
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST['year'])) {
+            $anno = "";
+        }
+        else {
+            $anno = test_input($_POST['year']);
+        }
+    }
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST['length'])) {
+            $lunghezza = "";
+        }
+        else {
+            $lunghezza = test_input($_POST['length']);
+        }
+    }
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST['location'])) {
+            $posizioneErr = "Richiesto";
+        }
+        else {
+            $posizione = test_input($_POST['location']);
+        }
+    }
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST['description'])) {
+            $descrizione = "";
+        }
+        else {
+            $descrizione = test_input($_POST['description']);
+        }
+    }
 
-$conn->close();
+    //$pwd = test_input($_POST['pwd']);   
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $pwd = test_input($_POST['pwd']);
+        if($pwd !== "password") {
+            $pwdErr = "Password errata!";
+        }
+        else {
+            $sql = 'SELECT id FROM oggetto WHERE id = ?';
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param($id);
+            $stmt->execute();
+            $stmt->fetch();
+            if($stmt->num_rows >=1) {
+                //id già presente.
+                $idErr = "id già utilizzato";
+            }
+            else {
+                $stmt->close();
+                //fare INSERT
+                $sql = 'INSERT INTO oggetto(id, nome, marca, anno, lunghezza, posizione, descrizione)
+                    VALUES(?, ?, ?, ?, ?, ?, ?)';
+                if(empty($anno)) {
+                    $anno = null;
+                    echo $anno;
+                }
+                else
+                    echo "CHAHAHAHAH";
+
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("sssssss", $id, $nome, set_empty_to_null($marca), set_empty_to_null($anno), set_empty_to_null($lunghezza), set_empty_to_null($posizione), set_empty_to_null($descrizione));
+                if($stmt->execute() == TRUE) {
+                    $stmt->close();
+                    $conn->close();
+                    header('Location: http://store.believegroup.it/beSTORE/test.html');
+                }
+                else {
+                    echo "NON VA UN CAZZO DI NIENTE </br>";
+                    echo $stmt->error;
+                }
+            }
+
+        }
+    }
+    
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+        }
+    
+    function set_empty_to_null($data) {
+        if(empty($data))
+            return NULL;
+        else
+            return $data;
+    }
+    
 ?>
-</p>
+<h2>Aggiungi oggetto in magazzino</h2>
+<p><span class="error">* campi obbligatori</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        ID: <input type="text" name="id" value=" <?php echo $id;?> ">
+        <span class="error">*
+            <?php echo $idErr;?></span>
+        <br><br>
+        Nome: <input type="text" name="name" value="<?php echo $nome;?>">
+        <span class="error">*
+            <?php echo $nameErr;?></span>
+        <br><br>
+        Marca: <input type="text" name="brand" value="<?php echo $marca;?>">
+        <br><br>
+        Anno: <input type="number" name="year" value="<?php echo $anno;?>">
+        <br><br>
+        Lunghezza: <input type="number" name="length" value="<?php echo $lunghezza;?>">
+        <br><br>
+        Posizione: <input type="text" name="location" value="<?php echo $posizione;?>">
+        <span class="error">*
+            <?php echo $posizioneErr;?></span>
+        <br><br>
+        Descrizione: <textarea name="description" rows="5" cols="40" value="<?php echo $descrizione;?>"></textarea>
+        <br><br>
+        PASSWORD: <input type="password" name="pwd">
+        <span class="error">* <?php echo $pwdErr;?></span>
+
+        <input type="submit">
+    </form>
+
+</body>
+
+</html>
